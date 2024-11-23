57ec233a48f68becdd15605cb7bd8b956459cffc
jpt1729 <john.tan-aristy@csedge.org>
2024-11-23 14:48:55 -0600

    updates

--- player.gd
+++ player.gd
@@ -15,7 +15,7 @@ func _process(delta: float) -> void:
 	input.x = Input.get_axis("move_left", "move_right")
 	input.z = Input.get_axis("move_forward", "move_back")
 	
-	apply_central_force($TwistPivot.basis * input * 1200.0 * delta)
+	apply_central_force($TwistPivot.basis * input * 1500.0 * delta)
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
