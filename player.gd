72f5837a4d46e884a30e694c3a174db81538b059
jpt1729 <john.tan-aristy@csedge.org>
2024-11-23 14:35:08 -0600

    update

--- player.gd
+++ player.gd
@@ -1,13 +1,35 @@
 extends RigidBody3D
 
+var mouse_sensitivity := 0.001
+var twist_input := 0.0
+var pitch_input := 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var input = Input.get_action_strength("ui_up")
-	apply_central_force(input * Vector3.FORWARD * 1200.0 * delta)
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_back")
 	
+	apply_central_force($TwistPivot.basis * input * 1200.0 * delta)
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	$TwistPivot.rotate_y(twist_input)
+	$TwistPivot/PitchPivot.rotate_x(pitch_input)
+	$TwistPivot/PitchPivot.rotation.x = clamp(
+		$TwistPivot/PitchPivot.rotation.x,
+		-0.5,
+		0.5
+	)
+	twist_input = 0.0
+	pitch_input = 0.0
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			twist_input = - event.relative.x * mouse_sensitivity
+			pitch_input = - event.relative.y * mouse_sensitivity
